f6fe16b9d93b2ebbf4b7c6a746c3d1af9c2c91f7
Anilton Francisco de Morais Junior <anilton.morais@bovexo.com>
2020-10-28 22:52:06 -0300

    Finalizando desafio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,74 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  let body = request.body;
+  body = {
+    id: uuid(),
+    ...body,
+    likes: 0
+  };
+  repositories.push(body);
+  return response.status(201).json(body);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const paramId = request.params.id;
+  if(!isUuid(paramId)){
+    return response.status(400).json({"error": "Param is not UUID type"});
+  } else {
+    const {id, likes, ...bodyWithouLikesAndId} = request.body;
+    const repositoryFoundedIndex = repositories.findIndex(r => r.id === paramId);
+    if(repositoryFoundedIndex < 0){
+      return response.status(400).json({"error": "Repository not founded"});
+    } else {
+      const repositoryFounded = repositories[repositoryFoundedIndex];
+      const updatedRepository = {
+        ...repositoryFounded,
+        ...bodyWithouLikesAndId
+      };
+      repositories[repositoryFoundedIndex] = updatedRepository;
+      return response.status(200).json(updatedRepository);
+    }
+  }
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const paramId = request.params.id;
+  if(!isUuid(paramId)){
+    return response.status(400).json({"error": "Param is not UUID type"});
+  } else {
+    const repositoryFoundedIndex = repositories.findIndex(r => r.id === paramId);
+    if(repositoryFoundedIndex < 0){
+      return response.status(400).json({"error": "Repository not founded"});
+    } else {
+      repositories.splice(repositoryFoundedIndex, 1);
+      return response.status(204).json({message: "Repository deleted"});
+    }
+  }
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const paramId = request.params.id;
+  if(!isUuid(paramId)){
+    return response.status(400).json({"error": "Param is not UUID type"});
+  } else {
+    const repositoryFoundedIndex = repositories.findIndex(r => r.id === paramId);
+    if(repositoryFoundedIndex < 0){
+      return response.status(400).json({"error": "Repository not founded"});
+    } else {
+      const repositoryFounded = repositories[repositoryFoundedIndex];
+      const updatedRepository = {
+        ...repositoryFounded,
+        likes: repositoryFounded.likes + 1
+      };
+      repositories[repositoryFoundedIndex] = updatedRepository;
+      return response.status(200).json(updatedRepository);
+    }
+  }
 });
 
 module.exports = app;
